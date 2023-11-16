41b015e3434e5b0844461717a3c4c2ea9e3fa95c
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-16 03:50:46 +0100

    Create Puppet manifest to generate /tmp/school file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,15 @@
-# Using puppet, create a file with below specifications
+# 0-create_a_file.pp
 
+# Ensure the /tmp directory exists
+file { '/tmp':
+  ensure => directory,
+}
+
+# Create the /tmp/school file with specified permissions, owner, and content
 file { '/tmp/school':
   ensure  => file,
-  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
