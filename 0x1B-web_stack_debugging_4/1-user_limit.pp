1c25ba2ff291c3adc09567bd1771355de0611acc
fazzy12 <kaluifeanyi00@gmail.com>
2024-01-02 07:37:41 +0100

    Fix "Too many open files" issue for holberton user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,15 @@
+# 1-user_limit.pp
+
+# Increase file descriptor limit for holberton user
+exec { 'increase_file_limit_for_holberton':
+  command => 'echo "holberton soft nofile 65535" >> /etc/security/limits.conf && echo "holberton hard nofile 65535" >> /etc/security/limits.conf',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'grep -q "^holberton" /etc/security/limits.conf',
+}
+
+# Reload session to apply new limits
+exec { 'reload_session':
+  command     => 'su - holberton -c "exec bash"',
+  refreshonly => true,
+  subscribe   => Exec['increase_file_limit_for_holberton'],
+}
